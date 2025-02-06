013ea706adde975d5d425a2c83f42d0f95bb5b28
christo <christo.pb@caddayn.com>
2025-02-06 05:12:18 -0500

    Update: updated version

--- lib/builder.dart
+++ lib/builder.dart
@@ -4,6 +4,10 @@ import 'dart:io';
 import 'package:build/build.dart';
 
 class TxtFileGenerator implements Builder {
+  final bool isEnabled;
+
+  TxtFileGenerator(this.isEnabled);
+
   @override
   final buildExtensions = const {
     r'$package$': [filePath],
@@ -13,22 +17,54 @@ class TxtFileGenerator implements Builder {
 
   @override
   Future<void> build(BuildStep buildStep) async {
+    if (!isEnabled) return;
+
     File file = File(filePath);
+    bool isEmpty = true;
 
     if (file.existsSync()) {
+      isEmpty = false;
       List<String> contents = file.readAsLinesSync();
+      contents = contents.where((e) => e.trim().isNotEmpty).toList();
+      if (contents.isEmpty) isEmpty = true;
     }
 
-    await buildStep.writeAsString(
-      AssetId(buildStep.inputId.package, filePath),
-      '',
-    );
+    if (isEmpty) await buildStep.writeAsString(AssetId(buildStep.inputId.package, filePath), contentString);
   }
 
   static String contentString = '''
+import 'package:dart_helpers/dart_helpers.dart';
 
+class AppVersionUtils {
+  static CVersion _version = CVersion(maj: 0, min: 0, patch: 0, build: 0);
+  static CVersion _pubVersion = CVersion(maj: 0, min: 0, patch: 0, build: 0);
+
+  static void setPubspecVersion(CVersion version) async {
+    _pubVersion = _pubVersion.copyWith(
+      maj: version.maj,
+      min: version.min,
+      patch: version.patch,
+      build: version.build,
+    );
+  }
+
+  static void setCVersion(CVersion version) async {
+    _version = _version.copyWith(
+      maj: version.maj,
+      min: version.min,
+      patch: version.patch,
+      build: version.build,
+    );
+  }
+
+  static CVersion getCVersion() => _version;
+  static CVersion getPubVersion() => _pubVersion;
+}
   ''';
 }
 
 /// Factory function for build.yaml
-Builder txtFileBuilder(BuilderOptions options) => TxtFileGenerator();
+Builder txtFileBuilder(BuilderOptions options) {
+  final isEnabled = options.config['enabled'] as bool? ?? false;
+  return TxtFileGenerator(isEnabled);
+}
