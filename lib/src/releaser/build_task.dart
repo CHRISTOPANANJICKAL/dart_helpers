e2eb99831422452b4d2c708825925a55b26f4fc1
christo <christo.pb@caddayn.com>
2025-02-07 01:11:16 -0500

    Update: Added build method to builder

--- lib/src/releaser/build_task.dart
+++ lib/src/releaser/build_task.dart
@@ -1,5 +1,7 @@
 // Created by: Christo Pananjickal, Created at: 07-02-2025 12:38 am
 
+import 'dart:io';
+
 import 'package:dart_helpers/dart_helpers.dart';
 
 class BuildTask {
@@ -11,12 +13,47 @@ class BuildTask {
       return false;
     }
 
-    if (result.output != null) return result.output!.contains(flutterVersion);
+    if (result.output == null) return false;
 
-    return false;
+    return result.output!.contains(flutterVersion);
   }
 
-  static Future<String?> buildExeFile() async {
-    await ProcessRunner().runProcess('flutter build ');
+  static Future<String?> buildExeFile({
+    required String outputName,
+    required String? flutterVersion,
+    bool branchCheck = true,
+  }) async {
+    if (branchCheck) {
+      bool correctBranch = await GitTasks.askCurrentBranchName();
+      if (!correctBranch) return 'Wrong branch name';
+    }
+
+    if (flutterVersion != null) {
+      bool correctVersion = await checkFlutterVersion(flutterVersion: flutterVersion);
+      if (!correctVersion) return 'Flutter version is not $flutterVersion';
+    }
+
+    ProcessOutput op = await ProcessRunner().runProcess('flutter clean');
+    if (!op.success) return op.error;
+
+    op = await ProcessRunner().runProcess('flutter pub get');
+    if (!op.success) return op.error;
+
+    op = await ProcessRunner().runProcess('dart run build_runner build --delete-conflicting-outputs');
+    if (!op.success) return op.error;
+
+    op = await ProcessRunner().runProcess('flutter build windows --release --obfuscate --split-debug-info=debug-info');
+    if (!op.success) return op.error;
+
+    Directory directory = Directory('debug-info');
+    if (directory.existsSync()) {
+      Commander.doing('Deleting debug info');
+      directory.deleteSync(recursive: true);
+    }
+
+    final outputFile = File('build/windows/x64/runner/Release/$outputName');
+    if (!outputFile.existsSync()) return 'Build failed. Output file was not generated';
+
+    return null;
   }
 }
