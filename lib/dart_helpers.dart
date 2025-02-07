440d47466f09c7288f138db28bcb364aa27e0ecf
christo <christo.pb@caddayn.com>
2025-02-06 20:57:56 -0500

    Update: Added commander.dart

--- lib/dart_helpers.dart
+++ lib/dart_helpers.dart
@@ -2,5 +2,6 @@ library;
 
 export 'builder.dart';
 export 'src/process_runner/process_runner.dart';
+export 'src/releaser/commander.dart';
 export 'src/version_utils/c_version.dart';
 export 'src/vlogger/vlogger.dart';
---
+++ lib/src/releaser/commander.dart
@@ -0,0 +1,41 @@
+// Created by: Christo Pananjickal, Created at: 06-02-2025 08:26 pm
+
+import 'dart:io';
+
+import 'package:dart_helpers/dart_helpers.dart';
+
+class Commander {
+  static Vlogger vlogger = Vlogger();
+
+  static void say(String text, {VClr color = VClr.say}) => vlogger.log(text, color: color.color);
+
+  static void doing(String text) => say(text, color: VClr.action);
+
+  static String ask(String question, {VClr color = VClr.question}) {
+    vlogger.log(question, color: color.color);
+    return (stdin.readLineSync() ?? '').trim();
+  }
+
+  static bool askYN(String question, {VClr color = VClr.question}) {
+    vlogger.log('$question(y/n)', color: color.color);
+    String? input = (stdin.readLineSync() ?? '').trim();
+    if (input == 'y' || input == 'Y') return true;
+    if (input == 'n' || input == 'N') return false;
+    vlogger.log('Invalid input', color: VlogColors.red);
+
+    return askYN(question, color: color);
+  }
+
+  static bool askYNSub(String question) => askYN('    $question', color: VClr.subQuestion);
+}
+
+enum VClr {
+  question(color: VlogColors.magenta),
+  subQuestion(color: VlogColors.green),
+  say(color: VlogColors.yellow),
+  action(color: VlogColors.grey),
+  ;
+
+  const VClr({required this.color});
+  final VlogColors color;
+}
